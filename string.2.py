15f6f12f909547bf0e871973e32cab39f659f76e
Joanne1-1 <joanne21pillay@gmail.com>
2025-06-20 20:19:07 +0200

    Add files via upload

---
+++ string.2.py
@@ -0,0 +1,9 @@
+
+message = ' Hello, World!'
+
+print(message.strip())# Remove leading and trailing whitespaces
+print(message.lower())#Convert all characters to lowercase
+print(message.split(',')) # Split the string into a list on the comma
+print(message.upper())#Convert all cahracters to uppercase
+print(message.replace('World','Universe'))#Replace 'World' with 'Universe'
+
